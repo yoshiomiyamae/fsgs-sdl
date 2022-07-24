f38e5cca9e1508fd7dd39b459369e729d899bfba
yoshiomiyamae <yoshio.miyamae@famoce-succellion.com>
2022-07-24 11:15:49 +0900

    add split function

--- common.hpp
+++ common.hpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <vector>
+
 namespace FSGS
 {
   template <typename Cont, typename Pred>
@@ -17,5 +20,51 @@ namespace FSGS
       return default;
     }
     return std::any_cast<const Ret &>(ret->second);
+  }
+
+  template <class T>
+  std::vector<std::string> split(const std::string &s, const T &separator, bool ignore_empty = 0, bool split_empty = 0)
+  {
+    struct
+    {
+      auto len(const std::string &s) { return s.length(); }
+      auto len(const std::string::value_type *p) { return p ? std::char_traits<std::string::value_type>::length(p) : 0; }
+      auto len(const std::string::value_type c) { return c == std::string::value_type() ? 0 : 1; /*return 1;*/ }
+    } util;
+
+    if (s.empty())
+    { /// empty string ///
+      if (!split_empty || util.len(separator))
+        return {""};
+      return {};
+    }
+
+    auto v = std::vector<std::string>();
+    auto n = static_cast<std::string::size_type>(util.len(separator));
+    if (n == 0)
+    { /// empty separator ///
+      if (!split_empty)
+        return {s};
+      for (auto &&c : s)
+        v.emplace_back(1, c);
+      return v;
+    }
+
+    auto p = std::string::size_type(0);
+    while (1)
+    { /// split with separator ///
+      auto pos = s.find(separator, p);
+      if (pos == std::string::npos)
+      {
+        if (ignore_empty && p - n + 1 == s.size())
+          break;
+        v.emplace_back(s.begin() + p, s.end());
+        break;
+      }
+      if (!ignore_empty || p != pos)
+        v.emplace_back(s.begin() + p, s.begin() + pos);
+      p = pos + n;
+    }
+    return v;
   }
 }
